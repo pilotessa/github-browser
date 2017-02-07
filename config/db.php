85c6d6a951964717dd5c940ab6b3ed3f23ce71e9
pilotessa <pilotessa@gmail.com>
2017-02-07 18:35:18 +0200

    Remove db settings from git

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=github_browser_new',
     'username' => 'root',
-    'password' => '',
+    'password' => '123',
     'charset' => 'utf8',
 ];
