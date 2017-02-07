41e7803c1bfa6fe936427561ef57416c3c394e98
pilotessa <pilotessa@gmail.com>
2017-02-07 15:57:56 +0200

    Display contributor details

--- controllers/ContributorController.php
+++ controllers/ContributorController.php
@@ -2,11 +2,33 @@
 
 namespace app\controllers;
 
-class ContributorController extends \yii\web\Controller
+use Yii;
+use yii\web\Controller;
+use yii\web\HttpException;
+use Github\Client;
+use RuntimeException;
+
+class ContributorController extends Controller
 {
+    /**
+     * Displays contributor details.
+     *
+     * @return string
+     * @throws HttpException
+     */
     public function actionIndex()
     {
-        return $this->render('index');
+        $request = Yii::$app->request;
+        $name = $request->get('name');
+
+        try {
+            $client = new Client();
+            $contributor = $client->api('user')->show($name);
+        } catch (RuntimeException $e) {
+            throw new HttpException(404, $e->getMessage());
+        }
+
+        return $this->render('index', compact('contributor'));
     }
 
 }
--- views/contributor/index.php
+++ views/contributor/index.php
@@ -1,9 +1,50 @@
 <?php
-/* @var $this yii\web\View */
-?>
-<h1>contributor/index</h1>
 
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+use app\components\LikeButtonWidget;
+
+$this->title = !empty($contributor['name']) ? $contributor['name'] : $contributor['login'];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-user">
+    <?php if (!empty($contributor['name'])): ?>
+        <h1><?= Html::encode($contributor['name']) ?></h1>
+    <?php endif; ?>
+    <p class="lead"><?= Html::encode($contributor['login']) ?></p>
+    <div class="row">
+        <div class="col-md-3">
+            <?php if (!empty($contributor['avatar_url'])): ?>
+                <p>
+                    <?= Html::img($contributor['avatar_url'], ['class' => 'img-rounded img-responsive', 'alt' => $contributor['login']]) ?>
+                </p>
+            <?php endif; ?>
+        </div>
+        <div class="col-md-9">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <dl class="dl-horizontal">
+                        <?php if (!empty($contributor['company'])): ?>
+                            <dt>Company</dt>
+                            <dd><?= Html::encode($contributor['company']) ?></dd>
+                        <?php endif; ?>
+                        <?php if (!empty($contributor['blog'])): ?>
+                            <dt>Blog</dt>
+                            <dd>
+                                <?= Html::a(Html::encode($contributor['blog']), $contributor['blog'], ['target' => '_blank']) ?>
+                            </dd>
+                        <?php endif; ?>
+                        <?php if (!empty($contributor['followers'])): ?>
+                            <dt>Followers</dt>
+                            <dd><?= Html::encode($contributor['followers']) ?></dd>
+                        <?php endif; ?>
+                    </dl>
+                </div>
+                <div class="panel-footer text-right">
+                    <?= LikeButtonWidget::widget() ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
