a204c040a5b498bc3f551ed417241833e1348fc1
pilotessa <pilotessa@gmail.com>
2017-02-08 10:05:15 +0200

    Untrack db settings

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=github_browser_new',
-    'username' => 'root',
-    'password' => '123',
-    'charset' => 'utf8',
-];
